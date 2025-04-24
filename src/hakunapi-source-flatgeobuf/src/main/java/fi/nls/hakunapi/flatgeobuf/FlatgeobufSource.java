f5f250bc6070cd96ccaf3b6137524031d2516453
jampukka <janne.z.heikkila@gmail.com>
2025-04-24 21:58:36 +0300

    Add support for relative files

--- src/hakunapi-source-flatgeobuf/src/main/java/fi/nls/hakunapi/flatgeobuf/FlatgeobufSource.java
+++ src/hakunapi-source-flatgeobuf/src/main/java/fi/nls/hakunapi/flatgeobuf/FlatgeobufSource.java
@@ -38,25 +38,30 @@ public class FlatgeobufSource implements SimpleSource {
         // Current prefix for properties
         String p = "collections." + collectionId + ".";
 
-        File file = new File(cfg.get(p + "db"));
-        
+        File file = getFile(cfg.get(p + "db"), path);
+
         int cfgSrid = Integer.parseInt(cfg.get(p + "srid.storage", "0"));
-        
+
         FlatgeobufFeatureType ft = new FlatgeobufFeatureType(file);
         ft.setName(collectionId);
         ft.setTitle(ft.meta.name);
-        
+
         ft.setId(getIdProperty(ft, cfg.getRequired(p + "id.mapping")));
         ft.setGeom(toHakunaGeometryProperty(collectionId, ft.meta, srids, cfgSrid));
         ft.setProperties(new ArrayList<>());
-        
+
         if (ft.getId() != null) {
             ft.constructIdIndex();
         }
 
         return ft;
     }
-    
+
+    private File getFile(String name, Path configPath) {
+        File file = new File(name);
+        return file.isAbsolute() ? file : new File(configPath.getParent().toFile(), name);
+    }
+
     private static HakunaProperty getIdProperty(FlatgeobufFeatureType ft, String idMapping) {
         String name = "id";
         boolean nullable = false;
@@ -64,8 +69,8 @@ public class FlatgeobufSource implements SimpleSource {
         ColumnMeta column = ft.meta.columns.stream().filter(it -> it.name.equalsIgnoreCase(idMapping)).findAny().get();
         HakunaPropertyType type = 
                 column.type == ColumnType.Int ? HakunaPropertyType.INT :
-                column.type == ColumnType.Long ? HakunaPropertyType.LONG :
-                column.type == ColumnType.String ? HakunaPropertyType.STRING : null;
+                    column.type == ColumnType.Long ? HakunaPropertyType.LONG :
+                        column.type == ColumnType.String ? HakunaPropertyType.STRING : null;
         String col = column.name;
         HakunaPropertyWriter propWriter = HakunaPropertyWriters.getIdPropertyWriter(ft, ft.getName(), name, type);
         switch (type) {
@@ -79,7 +84,7 @@ public class FlatgeobufSource implements SimpleSource {
             throw new IllegalArgumentException("Unsupported id column type");    
         }
     }
-    
+
     private static HakunaPropertyGeometry toHakunaGeometryProperty(String collectionId, HeaderMeta meta, int[] srids, int cfgSrid) {
         String name = "geometry";
         boolean nullable = false;
@@ -90,7 +95,7 @@ public class FlatgeobufSource implements SimpleSource {
         HakunaPropertyWriter propWriter = HakunaPropertyWriters.getGeometryPropertyWriter(name, true);
         return new HakunaPropertyGeometry(name, name, name, nullable, type, srids, storageSRID, dimension, propWriter); 
     }
-    
+
     private static int getSrid(String collectionId, int cfgSrid, int fileSrid) {
         if (cfgSrid <= 0 && fileSrid <= 0) {
             throw new IllegalArgumentException(String.format("Collection %s srid is unknown!", collectionId));
@@ -103,7 +108,7 @@ public class FlatgeobufSource implements SimpleSource {
             return fileSrid;
         }
     }
-    
+
     private static HakunaGeometryType toHakunaGeometryType(int geometryType) {
         switch (geometryType) {
         case GeometryType.Point:
@@ -124,5 +129,5 @@ public class FlatgeobufSource implements SimpleSource {
             throw new RuntimeException("Unknown geometry type");
         }
     }
-    
+
 }
